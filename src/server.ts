326c6f72f9a681b44ceeacd048ea36f16cc04288
abdullahriad-alghamdi <abdullahriad.alghamdi@gmail.com>
2025-08-09 01:10:18 +0300

    Fix: update server.ts to improve functionality

--- src/server.ts
+++ src/server.ts
@@ -29,20 +29,16 @@ app.use(express.urlencoded({ extended: true }))
 // app.use(myLogger)
 app.use(cookieParser())
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', dev.corsOrigin)
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-
-  connectDB() // connect mongo database
-  next()
-})
+// Setup allowed origins for CORS
+const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [];
 app.use(
   cors({
-    origin: dev.corsOrigin,
+    origin: allowedOrigins,
     credentials: true,
   })
 )
+
+connectDB(); // connect mongo database
 
 // Use routes
 app.use('/public', express.static('public'))
